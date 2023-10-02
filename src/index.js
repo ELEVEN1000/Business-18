942b774073039471fdd21e67644c5aaa64614081
nboy4 <nboy4521@gmail.com>
2023-10-02 16:26:38 +0700

    Faqs готов

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './styles/index.css';
 
-const acc = document.getElementsByClassName("accordion");
+const acc = document.getElementsByClassName("faqs__content-questions-btn");
 let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("faqs__content-questions-btn_active");
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
